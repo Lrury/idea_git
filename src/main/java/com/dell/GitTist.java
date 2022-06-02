2e27fe0265bcedf25ab3bfbcd1528ab698770d97
Lruri <321594699@qq.com>
2022-06-02 09:30:26 +0800

    第二次更新

--- src/main/java/com/dell/GitTist.java
+++ src/main/java/com/dell/GitTist.java
@@ -4,5 +4,6 @@ public class GitTist {
     public static void main(String[] args) {
         System.out.println("hello"  );
         System.out.println("第一次修改" );
+        System.out.println("第二次更新");
     }
 }
